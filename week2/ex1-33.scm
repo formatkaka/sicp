1e2e300fd8f264e38dbd2b9e1350cdaf5d41de1b
Siddhant <formatkaka@gmail.com>
2019-10-10 14:17:43 +0530

    finish ex1-33

--- week2/ex1-33.scm
+++ week2/ex1-33.scm
@@ -1,16 +1,19 @@
 (define (filtered-accumulate op default-val filter a b next term)
-  (if (filter a b)
+  
+  (if (> a b)
       0
-      (op (term a)
-	  	  (filtered-accumulate op default-val filter (next a) b next term))
+      (if (filter a b)
+          (op 0 (filtered-accumulate op default-val filter (next a) b next term))
+          (op (term a) (filtered-accumulate op default-val filter (next a) b next term))
       )
   )
-
+)
 
 ; sum of n natural prime numbers
 (define (prime? x div)
 
-   (cond
+  (cond
+    ((= x 1) false)
     ((= div x) true)
     ((= (modulo x div) 0) false)
     (else (prime? x (+ div 1)))
@@ -19,13 +22,19 @@
 )
 
 (define (primeFilter a b)
-  (if (> a b)
+  (if (prime? a 2)
       false
-      (prime? a 2))
+      true
+  )
 )
 
 (define (inc x) (+ x 1))
 
 (define (identity x) x)
 
-(filtered-accumulate + 0 primeFilter 3 10 inc identity)
+(filtered-accumulate + 0 primeFilter 1 10 inc identity)
+
+
+;; 3 - 10
+;; SUM(3, 5, 7) = 15
+
